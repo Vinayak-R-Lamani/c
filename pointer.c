7a4cc53854e77021959b8454db6a72d47a1b4148
project <rajulamani9888@gmail.com>
2024-07-31 00:27:37 +0530

    pointer

--- pointer.c
+++ pointer.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
+
+int returning_5(int*);
+void change_to_time(int*);
+// int* sum(int ,int );
+
+int* sum(int a,int b){
+       int c = a+ b;
+       int* ptr = &c;
+       printf("the sum is %d \n",c);
+       return &ptr;
+}
+
+
+ 
 int main()
 {
-       int i = 6;
-       int* j = &i;
-       int** k =&j;
-
-       printf("%d \n",i);
-       printf("%d \n",*j);
-       printf("%d \n",*k);
+     int i = 2;
+     sum(10,10);
+     printf("%u\n",&i);
+     int* ptr = sum(10,10);
+     printf("%u\n",ptr);
        
 
 } 
